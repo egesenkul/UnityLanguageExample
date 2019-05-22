0dd82d6010d0e0f2c04abcd5fec71f8e239fc309
Ege ŞENKUL <egesenkul35@hotmail.com>
2019-05-22 21:05:20 +0300

    Language Support Added

--- LanguageExample/Assets/Scripts/ButtonScipt.cs
+++ LanguageExample/Assets/Scripts/ButtonScipt.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonScipt : MonoBehaviour
 {
+    Language languageScript;
+
+    void Start()
+    {
+        languageScript = GameObject.Find("Main Camera").GetComponent<Language>();
+        GetAllText();
+    }
+
     public void PlayButtonClick()
     {
         print("PLAY");
@@ -11,11 +20,39 @@ public class ButtonScipt : MonoBehaviour
 
     public void SettingsClick()
     {
-        print("İLKİM");
+        print("SETTINGS");
     }
 
     public void InfoClick()
     {
         print("INFO");
+    }
+
+    public void TurkceClick()
+    {
+        languageScript.language = "tr";
+        PlayerPrefs.SetString("language", "tr");
+        GetAllText();
+    }
+
+    public void IngilizceClick()
+    {
+        languageScript.language = "en";
+        PlayerPrefs.SetString("language", "en");
+        GetAllText();
+    }
+
+    public void FransizcaClick()
+    {
+        languageScript.language = "fr";
+        PlayerPrefs.SetString("language", "fr");
+        GetAllText();
+    }
+
+    private void GetAllText()
+    {
+        GameObject.Find("btnPlay").GetComponentInChildren<Text>().text = languageScript.GetText("play");
+        GameObject.Find("btnSettings").GetComponentInChildren<Text>().text = languageScript.GetText("settings");
+        GameObject.Find("btnInfo").GetComponentInChildren<Text>().text = languageScript.GetText("info");
     }
 }
---
+++ LanguageExample/Assets/Scripts/Language.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Language : MonoBehaviour
+{
+    public string language;
+
+    Dictionary<string, string> TurkceTextler;
+    Dictionary<string, string> IngilizceTextler;
+    Dictionary<string, string> FransizcaTextler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       language = PlayerPrefs.GetString("language", "tr");
+
+        SetTextler();
+        SetTurkce();
+        SetIngilizce();
+        SetFransizca();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetTextler()
+    {
+        TurkceTextler = new Dictionary<string, string>();
+        IngilizceTextler = new Dictionary<string, string>();
+        FransizcaTextler = new Dictionary<string, string>();
+    }
+
+    void SetTurkce()
+    {
+        TurkceTextler.Add("play", "OYNA");
+        TurkceTextler.Add("info", "BİLGİ");
+        TurkceTextler.Add("settings", "AYARLAR");
+    }
+
+    void SetIngilizce()
+    {
+        IngilizceTextler.Add("play", "PLAY");
+        IngilizceTextler.Add("info", "INFO");
+        IngilizceTextler.Add("settings", "SETTINGS");
+    }
+
+    void SetFransizca()
+    {
+        FransizcaTextler.Add("play", "JOUER");
+        FransizcaTextler.Add("info", "LÖ BİLGİ");
+        FransizcaTextler.Add("settings", "LÖ SETTİNGS");
+    }
+
+    public string GetText(string key)
+    {
+        string result = string.Empty;
+        switch (language)
+        {
+            case "tr":
+                result = TurkceTextler[key];
+                break;
+            case "en":
+                result = IngilizceTextler[key];
+                break;
+            case "fr":
+                result = FransizcaTextler[key];
+                break;
+        }
+        return result;
+    }
+}
